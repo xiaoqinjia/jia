022ef1b19aca5d34ce268575c59f90607ee53a8c
conted <conted@BH210-03.studentservers.JAC-SERVERS.JOHNABBOTT.QC.CA>
2018-05-26 13:39:10 -0400

    my lecture 1 notes

---
+++ lecture1/MainClass.java
@@ -0,0 +1,7 @@
+public class MainClass {
+    public static void main(String[] args){
+        System.out.println("Hello world");
+    }
+        public int printSum(int x, int y){
+         return x+y;}
+}
